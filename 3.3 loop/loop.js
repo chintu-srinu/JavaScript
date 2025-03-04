6bfd7730dc430ebd03b727fa109009b1362c26ed
chintu-srinu <budigesrinivas885@gmail.com>
2025-03-04 12:05:38 +0530

    Add files via upload

---
+++ 3.3 loop/loop.js
@@ -0,0 +1,44 @@
+for (i=1;i<=10;i++){
+    console.log(i);
+}
+
+task-2
+for ( var i=1;i<=100;i++){
+    console.log(i);
+}
+
+task-3
+var table = 7
+for (i=1;i<=10;i++){
+       console.log(`${table} x ${i} = ${table*i}`)
+}
+
+task-4
+for ( var i=2;i<=20;i+=2){
+    console.log(i);
+}
+
+task=7
+for ( var i=19;i>=1;i-=1){
+    console.log(i);
+}
+task-6
+
+let base = 2;    
+let exponent = 5;
+let result = 1; 
+for (let i = 0; i < exponent; i++) {
+    result *= base;
+}
+
+console.log(`${base}^${exponent} =`, result);
+
+task-5
+
+let num = 5; 
+let factorial = 1;
+for (let i = 1; i <= num; i++) {
+    factorial *= i;
+}
+
+console.log(`${num}! =`, factorial);
